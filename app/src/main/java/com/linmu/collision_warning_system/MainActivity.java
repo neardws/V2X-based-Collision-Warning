c6b5f8b5b78e646de9f701b70bf1a456cca86b7f
LinMu-z <linmu.cqu@qq.com>
2023-02-17 10:23:18 +0800

    build: 将百度SDK由gradle管理替换为本地包

--- app/src/main/java/com/linmu/collision_warning_system/MainActivity.java
+++ app/src/main/java/com/linmu/collision_warning_system/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
